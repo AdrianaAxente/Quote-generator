817360d7fea15e7d97854ce1206f4c9e0d8cd859
AdrianaAxente <109233236+AdrianaAxente@users.noreply.github.com>
2022-10-20 16:46:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const redditBtn = document.getElementById("reddit");
+const newQuoteBtn = document.getElementById("Another source of motivation");
+const loader = document.getElementById('loader')
+
 let apiQuotes = [];
+
+// Show Loading
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
 
 // Show New Quote
 function newQuote() {
+	loading();
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote);
+	// Chef If Author Field is blank and replace it with "Unknown"
+	if (!quote.author) {
+		authorText.textContent = "Unknown";
+	} else {
+		authorText.textContent = quote.author;	
+	}
+	// Check Quote length to determine styling
+	if (quote.text.length > 100) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	// Set Quote, Hide Loader
+	quoteText.textContent = quote.text;
+	complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+	loading();
 	const apiUrl = 'https://type.fit/api/quotes';
 	try { 
 		const response = await fetch(apiUrl);
@@ -18,6 +53,17 @@ async function getQuotes() {
 		// Catch Error Here
 	}
 }
+
+// Reddit the Quote
+function redditQuote() {
+	const redditUrl = `https://www.reddit.com/submit?text=${quoteText.textContent}
+	- ${authorText.textContent}`;
+	window.open(redditUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+redditBtn.addEventListener('click', redditQuote);
 
 // On Load
 getQuotes();
